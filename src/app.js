af6455eb102d97ba1d185d702ffdaa96a866766d
elian-src <elianseverino117@gmail.com>
2026-03-03 08:12:37 -0400

    add files to upload page in server

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express()
+const port = process.env.port || 3000;
+
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.listen(port, () => {
+    console.log(`App listening on http://localhost:${port}`)
+})
